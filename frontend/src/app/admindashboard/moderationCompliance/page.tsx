218c8de41e6040d61813426c81b6194990a22f16
Iana <quides.299075@caloocan.sti.edu.ph>
2026-02-05 10:35:23 +0800

    fixed dashboard admin

---
+++ frontend/src/app/admindashboard/moderationCompliance/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+import ModerationCompliance from '../components/moderationComplieance';
+
+export default function ModerationCompliancePage() {
+    return (
+        <div className="min-h-screen bg-gray-100">
+            <div className="p-6">
+                <ModerationCompliance />
+            </div>
+        </div>
+    );
+}
+
